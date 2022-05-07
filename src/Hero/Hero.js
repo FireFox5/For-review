dc0efe823fab7dd9ae5d7b93aeb1929d655a3447
FireFox5 <sultanmuratnaspekov@gmail.com>
2022-05-07 19:35:22 +0600

    first

---
+++ src/Hero/Hero.js
@@ -0,0 +1,29 @@
+import React  from "react";
+import './Hero.css'
+
+
+
+const header =()=>{
+    return(
+       <div className='main' >
+            <h1>Hello it is my First homework!</h1>
+         <div className={'Head'}>
+            <p>It is beautiful when you understand </p>
+         </div>
+           <div>
+               <ul>
+                   <li><a href="#">You</a></li>
+                   <li><a href="#">Can</a></li>
+                   <li><a href="#">Choose</a></li>
+                   <li><a href="#">One</a></li>
+                   <li><a href="#">Of</a></li>
+                   <li><a href="#">Them</a></li>
+               </ul>
+           </div>
+        </div>
+    )
+
+}
+
+export default header;
+
